01d4be5a0748f3b41c914d49dff23bc0d1349ad8
KaiSeow <kaii4112000@gmail.com>
2022-09-15 03:24:56 +0800

    login

--- Trabot/Screens/MarketScreen.js
+++ Trabot/Screens/MarketScreen.js
@@ -1,62 +1,74 @@
-import React, {useRef, useMemo, useState, useEffect} from 'react';
-import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import ListItem from '../components/ListItem';
-import Chart from '../components/Chart';
+import React, { useRef, useMemo, useState, useEffect } from "react";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import ListItem from "../components/ListItem";
+import Chart from "../components/Chart";
 import {
   BottomSheetModal,
-  BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet';
-import { getMarketData } from '../services/cryptoService';
+  BottomSheetModalProvider
+} from "@gorhom/bottom-sheet";
+import { getMarketData } from "../services/cryptoService";
 
 const ListHeader = () => (
   <>
     <View style={styles.titleWrapper}>
-        <Text style={styles.largeTitle}>Markets</Text>
-      </View>
+      <Text style={styles.largeTitle}>Markets</Text>
+    </View>
     <View style={styles.divider} />
   </>
-)
+);
 
 export default function App() {
   const [data, setData] = useState([]);
   const [selectedCoinData, setSelectedCoinData] = useState(null);
 
+  // console.log(data);
   useEffect(() => {
     const fetchMarketData = async () => {
       const marketData = await getMarketData();
       setData(marketData);
-    }
+    };
 
     fetchMarketData();
-  }, [])
+  }, []);
 
   const bottomSheetModalRef = useRef(null);
 
-  const snapPoints = useMemo(() => ['50%'], []);
+  const snapPoints = useMemo(() => ["50%"], []);
 
-  const openModal = (item) => {
+  const openModal = item => {
     setSelectedCoinData(item);
+
     bottomSheetModalRef.current?.present();
-  }
+  };
+
+  // var newArray = data.filter(function (el) {
+  //   return el.name == "Tether";
+  // });
+
+  // console.log(newArray[0].current_price);//show current price of specific crypto from data array
+
+  // console.log(data[2].current_price); //show current price of 3rd object in data array
 
   return (
     <BottomSheetModalProvider>
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={data}
-        renderItem={({ item }) => (
-          <ListItem
-            name={item.name}
-            symbol={item.symbol}
-            currentPrice={item.current_price}
-            priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-            logoUrl={item.image}
-            onPress={() => openModal(item)}
-          />
-        )}
-        ListHeaderComponent={<ListHeader />}
-      />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          keyExtractor={item => item.id}
+          data={data}
+          renderItem={({ item }) => (
+            <ListItem
+              name={item.name}
+              symbol={item.symbol}
+              currentPrice={item.current_price}
+              priceChangePercentage7d={
+                item.price_change_percentage_7d_in_currency
+              }
+              logoUrl={item.image}
+              onPress={() => openModal(item)}
+            />
+          )}
+          ListHeaderComponent={<ListHeader />}
+        />
       </SafeAreaView>
 
       <BottomSheetModal
@@ -65,48 +77,51 @@ export default function App() {
         snapPoints={snapPoints}
         style={styles.bottomSheet}
       >
-        { selectedCoinData ? (
+        {selectedCoinData ? (
           <Chart
             currentPrice={selectedCoinData.current_price}
             logoUrl={selectedCoinData.image}
             name={selectedCoinData.name}
             symbol={selectedCoinData.symbol}
-            priceChangePercentage7d={selectedCoinData.price_change_percentage_7d_in_currency}
+            priceChangePercentage7d={
+              selectedCoinData.price_change_percentage_7d_in_currency
+            }
             sparkline={selectedCoinData?.sparkline_in_7d.price}
           />
         ) : null}
       </BottomSheetModal>
-      </BottomSheetModalProvider>
+    </BottomSheetModalProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   titleWrapper: {
     marginTop: 20,
-    paddingHorizontal: 16,
+    paddingHorizontal: 16
   },
   largeTitle: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: "bold"
   },
   divider: {
     height: StyleSheet.hairlineWidth,
-    backgroundColor: '#A9ABB1',
+    backgroundColor: "#A9ABB1",
     marginHorizontal: 16,
-    marginTop: 16,
+    marginTop: 16
   },
   bottomSheet: {
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: -4,
+      height: -4
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5,
-  },
+    elevation: 5
+  }
 });
--- Trabot/services/firebase.js
+++ Trabot/services/firebase.js
@@ -1,26 +1,22 @@
-// Import the functions you need from the SDKs you need
 import * as firebase from "firebase";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCAJmwarEsN2Ykldnw2sLgD0W4JQiM0ZY",
-  authDomain: "fyp2-e28e1.firebaseapp.com",
-  projectId: "fyp2-e28e1",
-  storageBucket: "fyp2-e28e1.appspot.com",
-  messagingSenderId: "791146732075",
-  appId: "1:791146732075:web:ecc89e09bbcc257cb3286e"
+  apiKey: "AIzaSyB8wzkiYeVekD26DxBdlyX1MXBMdUoKa9g",
+  authDomain: "trabot-e9c9f.firebaseapp.com",
+  projectId: "trabot-e9c9f",
+  storageBucket: "trabot-e9c9f.appspot.com",
+  messagingSenderId: "249002822374",
+  appId: "1:249002822374:web:f828986847e2ea08f90927"
+  // measurementId: "G-3JN0JQSQL2"
 };
 
 // Initialize Firebase
 let app;
-//firebase.initializeApp(firebaseConfig);
-if(firebase.apps.length === 0){
-    app = firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
 } else {
-    app = firebase.app()
+  app = firebase.app();
 }
-const auth = firebase.auth()
 
-export {auth};
+const auth = firebase.auth();
+export { auth };
